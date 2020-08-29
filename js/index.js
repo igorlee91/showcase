8b3318f59ab627f19bcd487bb7bac19997a6530a
igor <igorlee@mail.com>
2020-08-29 11:59:39 +0200

    new animations added

---
+++ js/index.js
@@ -0,0 +1,49 @@
+window.onload=function(){
+    document.querySelector("#over-nav").onclick = getOver
+    document.querySelector("#port-nav").onclick = getPortfolio
+    nav_hover();
+    function getOver(){
+        document.querySelector("#over-nav").className="transform active_nav"
+        document.querySelector("#port-nav").className="transform"
+            setTimeout(() => {
+                document.getElementsByClassName("main_over")[0].className = "main_over transform displayed"
+            }, 0);
+            
+            setTimeout(() => {
+                document.getElementsByClassName("main_port")[0].classList.add("center_toleft")
+            }, 50);
+            setTimeout(() => {
+                document.getElementsByClassName("main_over")[0].className = "main_over transform right_tocenter"
+            }, 50);
+    }
+    function getPortfolio(){
+        document.querySelector("#port-nav").className="transform active_nav"
+        document.querySelector("#over-nav").className="transform"
+        setTimeout(() => {
+            document.getElementsByClassName("main_over")[0].className = "main_over transform"
+        }, 50);
+        setTimeout(() => {
+            document.getElementsByClassName("main_port")[0].className = "main_port transform"
+        }, 50); 
+        const interval = setInterval(() => {
+            if (window.getComputedStyle(document.getElementsByClassName("main_over")[0], null).getPropertyValue("margin-right")==="0px") {
+                document.getElementsByClassName("main_over")[0].className = "main_over transform undisplayed"
+                clearInterval(interval)
+            }
+            console.log(window.getComputedStyle(document.getElementsByClassName("main_over")[0], null).getPropertyValue("margin-right"))
+        }, 200);
+        
+    }
+    function nav_hover(){
+        for(x of document.getElementsByClassName('navitem')){
+            x.addEventListener('mouseover',function(){
+                this.className = "navitem nav_active";
+            })
+            x.addEventListener('mouseout',function(){
+                if(this.id !== "portfolio-id"){
+                    this.className = "navitem nav_inactive";
+                }
+            })
+        }
+    }
+}
